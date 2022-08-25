820b5ef5bc24ba6037000000b82a0c6e4615ce5d
Mark Palencia <111488348+DwayneDantes@users.noreply.github.com>
2022-08-25 15:40:54 +0800

    Create character.java

---
+++ StandardClass/character.java
@@ -0,0 +1,21 @@
+public class Character {
+    int AttackDamage;
+    int AbilityPower;
+    int Armor;
+    String name ;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    
+    public void sayMyAttackDamage() {
+        System.out.println("My Attack Damage is " + AttackDamage); }
+    
+    public void sayMyAbilityPower() {
+        System.out.println("My Ability Power is  " + Armor); }
+    
+    public void sayMyArmor() {
+        System.out.println("My Armor is " + Armor);
+    }
+}
+
