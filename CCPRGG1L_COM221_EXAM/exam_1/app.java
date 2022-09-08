3a7bf3cc8b1bf30df6d945d34e67ec762178ad8c
Mark Palencia <111488348+DwayneDantes@users.noreply.github.com>
2022-09-08 14:12:35 +0800

    Update app.java

--- CCPRGG1L_COM221_EXAM/exam_1/app.java
+++ CCPRGG1L_COM221_EXAM/exam_1/app.java
@@ -1,1 +1,11 @@
 
+public class App {
+    public static void main(String[] args) {
+        student myself = new student("Palencia", "Mark Emmanuel", "C.", "12/16/2003", 2022103486,"palenciamc@students.national-u.edu.ph",true);
+        myself.sayMyStudentnumber();
+        myself.sayMyStudentEmailAdress();
+        myself.AmIAwesome();
+    }
+
+
+}
